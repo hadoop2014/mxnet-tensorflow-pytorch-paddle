64cbbe2d2daa14bf5c02d1b128e2d08d107ba647
hadoop2014 <wwyandotte@hotmial.com>
2019-11-04 23:02:58 +0800

    epoch_per_print放到configbase.txt中配置，对于所有的图像分类任务，该值设置为1,对于rnn等任务，可设置为其他值

--- datafetch/getPikachu.py
+++ datafetch/getPikachu.py
@@ -5,12 +5,12 @@ class getPikachuDataM(getdataBaseM):
         super(getPikachuDataM, self).__init__(gConfig)
 
     def load_data(self,root=""):
+        from mxnet import image
         root = os.path.expanduser(root)
-        self.train_data = self.dataset_selector[self.dataset_name](root=root,train=True)
-        self.test_data = self.dataset_selector[self.dataset_name](root=root,train=False)
+        train_data = self.dataset_selector[self.dataset_name](root=root,train=True)
+        test_data = self.dataset_selector[self.dataset_name](root=root,train=False)
         self.resizedshape = self.rawshape
-        #transformer = self.get_transformer(train=True)
-        '''self.train_data = image.ImageDetIter(
+        self.train_data = image.ImageDetIter(
             path_imgrec=train_data.path_rec,
             path_imgidx=train_data.path_idx,
             batch_size=self.batch_size,
@@ -23,41 +23,28 @@ class getPikachuDataM(getdataBaseM):
             path_imgrec=test_data.path_rec,
             batch_size=self.batch_size,
             data_shape=tuple(self.resizedshape),
-            shuffle=False)'''
+            shuffle=False)
 
-    def transform(self,reader):
+    def transform(self,reader,batch_size):
         def transform_reader():
             for batch in reader:
                 X = batch.data[0]
                 y = batch.label[0]
-                if y.shape[0] == self.batch_size:
+                if y.shape[0] == batch_size:
+                    #丢弃最后一个不足batch_size长度的batch,目的时保证计算accuracy正确
                     yield (X,y)
         return transform_reader
 
     @getdataBase.getdataForUnittest
     def getTrainData(self,batch_size):
-        from mxnet import image
-        train_iter = image.ImageDetIter(
-            path_imgrec=self.train_data.path_rec,
-            path_imgidx=self.train_data.path_idx,
-            batch_size=self.batch_size,
-            data_shape=tuple(self.resizedshape),
-            shuffle=True,  # 以随机顺序读取数据集
-            rand_crop=1,  # 随机裁剪的概率为1
-            min_object_covered=0.95,
-            max_attempts=200)
-        self.train_iter = self.transform(train_iter)
+        self.train_data.reset()
+        self.train_iter = self.transform(self.train_data,batch_size)
         return self.train_iter()
 
     @getdataBase.getdataForUnittest
     def getTestData(self,batch_size):
-        from mxnet import image
-        test_iter = image.ImageDetIter(
-            path_imgrec=self.test_data.path_rec,
-            batch_size=self.batch_size,
-            data_shape=tuple(self.resizedshape),
-            shuffle=False)
-        self.test_iter = self.transform(test_iter)
+        self.test_data.reset()
+        self.test_iter = self.transform(self.test_data,batch_size)
         return self.test_iter()
 
 class_selector = {
